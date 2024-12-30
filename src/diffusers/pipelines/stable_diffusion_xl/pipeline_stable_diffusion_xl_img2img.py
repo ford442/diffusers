879fe0e30255a8f906261ab943f0910166f8600e
Noah Cohn <noahc42@gmail.com>
2024-12-29 18:59:45 -0800

    Update pipeline_stable_diffusion_xl_img2img.py

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl_img2img.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl_img2img.py
@@ -360,6 +360,11 @@ class StableDiffusionXLImg2ImgPipeline(
                 Number of layers to be skipped from CLIP while computing the prompt embeddings. A value of 1 means that
                 the output of the pre-final layer will be used for computing the prompt embeddings.
         """
+        if self.text_encoder.device != "cuda":
+            self.text_encoder.to("cuda")
+        if self.text_encoder_2.device != "cuda":
+            self.text_encoder_2.to("cuda") 
+            
         device = device or self._execution_device
 
         # set lora scale so that monkey patched LoRA
@@ -534,6 +539,9 @@ class StableDiffusionXLImg2ImgPipeline(
                 # Retrieve the original scale by scaling back the LoRA layers
                 unscale_lora_layers(self.text_encoder_2, lora_scale)
 
+        self.text_encoder.to("cpu")
+        self.text_encoder_2.to("cpu")
+        
         return prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds
 
     # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.prepare_extra_step_kwargs
