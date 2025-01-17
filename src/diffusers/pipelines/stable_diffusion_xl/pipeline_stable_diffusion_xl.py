7a47652989b2063e557fb7ad87b6d7f990818414
Noah Cohn <noahc42@gmail.com>
2025-01-17 08:10:33 -0800

    Update pipeline_stable_diffusion_xl.py

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
@@ -1271,10 +1271,13 @@ class StableDiffusionXLPipeline(
                 )
                 latents = latents * latents_std / self.vae.config.scaling_factor + latents_mean
             else:
+                if self.vae.device.type == 'cpu':
+                    self.vae.to('cuda')
                 latents = latents / self.vae.config.scaling_factor
                 print('Changing latent/VAE to float32')
            #     self.vae.to(torch.float32)
                 latents.to(torch.float32)
+                
                 image = self.vae.decode(latents, return_dict=False)[0]
 
            # self.vae.to(dtype=torch.bfloat16)
